3397ed9ff9f8264d55a1983d75e0ecf1d66609d2
KarimKashkoush <karimkashkoush5@gmail.com>
2025-08-13 08:21:41 +0300

    add

--- server.js
+++ server.js
@@ -9,11 +9,23 @@ const app = express();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+      "https://shafi-d5v1x5yu5-karimkashkoushs-projects.vercel.app",
+];
+
 app.use(cors({
-      origin: "*",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: false
+      origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                  const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+                  return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+      },
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"]
 }));
+
 
 app.use(express.json());
 app.use('/', authRoutes);
